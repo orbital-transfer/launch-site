75bf47d989e03dbc57a576c3d1231fd2b353fa4e
Zakariyya Mughal <zaki.mughal@gmail.com>
2018-02-26 02:21:15 -0500

    Start CLI skeleton

--- lib/Oberth/Prototype.pm
+++ lib/Oberth/Prototype.pm
@@ -1,8 +1,23 @@
+use Modern::Perl;
 package Oberth::Prototype;
 
-use strict;
-use warnings;
+BEGIN {
+	require File::Glob;
+	require FindBin;
+	our @VENDOR_LIB = File::Glob::bsd_glob("$FindBin::Bin/../vendor/*/lib");
+	unshift @INC, @VENDOR_LIB;
+}
 
+use Moo;
+use CLI::Osprey;
+
+use Oberth::Common::Setup;
+
+subcommand 'list-native-dependencies' => method() { 1 };
+
+method run() {
+	...
+}
 
 
 1;
